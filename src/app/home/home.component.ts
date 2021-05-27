ede9c73a8862945fe5c63031c8324dfa2f60c8c8
jyyblue <jyyblue1987@outlook.com>
2021-05-27 12:23:24 +0800

    [add] show expenses.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Expenses {
+	expense: string;
+	cost: number;
+	priority: string;
+	desired: number;
+	actual: number;
+}
+
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
+	total_budget: number = 100.00;
 
-  constructor() { }
+	expenses: Expenses[] = [
+		{
+			expense: 'Food',
+			cost: 40,
+			priority: 'High',
+			desired: 30,
+			actual: 40
+		},
+		
+		{
+			expense: 'Netflix',
+			cost: 15,
+			priority: 'Medium',
+			desired: 15,
+			actual: 15
+		}
+	];
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void {
+
+	}
 
 }
